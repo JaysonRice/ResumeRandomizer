60faabb0c19d480f0585ef8d0aa0c8981edb2512
Jayson Rice <jayson.rice.95@gmail.com>
2021-02-02 15:08:34 -0500

    Resume provider fetch calls added.

---
+++ ResumeRandomizer/client/src/providers/ResumeProvider.js
@@ -0,0 +1,112 @@
+import React, { useState, useContext } from "react";
+import { UserProfileContext } from "./UserProfileProvider"
+import "firebase/auth";
+
+export const ReviewPostContext = React.createContext();
+
+export const ReviewPostProvider = (props) => {
+    const [resumes, setResumes] = useState([]);
+
+    const apiUrl = "/api/resume";
+    const { getToken } = useContext(UserProfileContext);
+
+    const getAllResumeList = () =>
+        getToken().then((token) =>
+            fetch(`${apiUrl}/resumelist`, {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then((res) => res.json())
+                .then(setResumes));
+
+    const getResume = (id) => {
+        return getToken().then((token) =>
+            fetch(apiUrl + `/${id}`, {
+                method: "Get",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+            }).then(resp => {
+                if (resp.ok) {
+                    return resp.json();
+                }
+                else { throw new Error("Unauthorized"); }
+            }));
+    }
+
+    const getUserResumes = (id) => {
+        getToken().then((token) =>
+            fetch(apiUrl + `/getbyuser/${id}`, {
+                method: "Get",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+            }).then(resp => {
+                if (resp.ok) {
+                    return resp.json().then(setResumes);
+                }
+                throw new Error("Unauthorized");
+            }))
+    };
+    
+    const addResume = (resume) =>
+        getToken().then((token) =>
+            fetch(apiUrl, {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(resume),
+            }).then(resp => {
+                if (resp.ok) {
+                    return resp.json();
+                }
+                throw new Error("Unauthorized");
+            }));
+
+    const deleteResume = (id) => {
+        return getToken().then((token) =>
+            fetch(apiUrl + `/${id}`, {
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then((resp) => {
+                if (resp.ok) {
+                    return;
+                }
+                throw new Error("Failed to delete post.")
+            })
+        );
+    };
+
+    const editResume = (id, resume) => {
+        return getToken().then((token) =>
+            fetch(apiUrl + `/${id}`, {
+                method: "PUT",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(resume),
+            }).then(resp => {
+                if (resp.ok) {
+                    return;
+                }
+                throw new Error("Unauthorized");
+            }))
+    };
+
+    return (
+        <ReviewPostContext.Provider value={{
+            resumes, getAllResumeList, getResume, getUserResumes, 
+            addResume, deleteResume, editResume
+        }}>
+            {props.children}
+        </ReviewPostContext.Provider>
+    );
+};
