4ff586c808857a021072932c2f1ee2ef83a238e8
Jayson Rice <jayson.rice.95@gmail.com>
2021-02-10 12:59:11 -0500

    Add education modal opens.

---
+++ ResumeRandomizer/client/src/components/education/AddEducationForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Form } from "semantic-ui-react";
+
+const AddEducationForm = () => {
+  return (
+    <>
+      <p>Education form here</p>
+      {/* <Form></Form> */}
+    </>
+  );
+};
+
+export default AddEducationForm;
--- ResumeRandomizer/client/src/components/education/EducationList.js
+++ ResumeRandomizer/client/src/components/education/EducationList.js
@@ -1,10 +1,15 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Button, Modal } from "semantic-ui-react";
 import { EducationContext } from "../../providers/EducationProvider";
+import AddEducationForm from "./AddEducationForm";
 import Education from "./Education";
 
 const EducationList = () => {
   const { education, getUserEducation } = useContext(EducationContext);
   const userProfile = JSON.parse(sessionStorage.userProfile);
+
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => setShowModal(!showModal);
 
   useEffect(() => {
     getUserEducation(userProfile.id);
@@ -17,9 +22,31 @@ const EducationList = () => {
   return (
     <>
       <h3>{userProfile.firstName}'s Education</h3>
+      <Button onClick={toggleModal}>New Education</Button>
       {education.map((education) => {
         return <Education key={education.id} education={education} />;
       })}
+
+      <Modal open={showModal} toggle={toggleModal}>
+        <Modal.Header toggle={toggleModal}>Add Education</Modal.Header>
+
+        <Modal.Content toggle={toggleModal}>
+          <AddEducationForm />
+        </Modal.Content>
+
+        <Modal.Actions>
+          <Button color="black" onClick={toggleModal}>
+            Cancel
+          </Button>
+          <Button
+            content="Save"
+            labelPosition="right"
+            icon="checkmark"
+            onClick={toggleModal}
+            positive
+          />
+        </Modal.Actions>
+      </Modal>
     </>
   );
 };
