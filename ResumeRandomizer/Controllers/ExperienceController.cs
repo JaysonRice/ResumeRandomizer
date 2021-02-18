02a01a6497a74a856574e41e3e6a4c47bb44b8f5
Jayson Rice <jayson.rice.95@gmail.com>
2021-02-18 13:04:53 -0500

    Repository and controller for experience built out.

---
+++ ResumeRandomizer/Controllers/ExperienceController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResumeRandomizer.Data;
+using ResumeRandomizer.Models;
+using ResumeRandomizer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ResumeRandomizer.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExperienceController : ControllerBase
+    {
+        private readonly ExperienceRepository _experienceRepository;
+        private readonly UserProfileRepository _userProfileRepository;
+        public ExperienceController(ApplicationDbContext context)
+        {
+            _experienceRepository = new ExperienceRepository(context);
+            _userProfileRepository = new UserProfileRepository(context);
+        }
+
+        [HttpGet("getbyuser/{id}")]
+        public IActionResult GetExperienceByUser(int id)
+        {
+            return Ok(_experienceRepository.GetByUserProfileId(id));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var experience = _experienceRepository.GetById(id);
+            if (experience == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(experience);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Experience experience)
+        {
+            _experienceRepository.Add(experience);
+            return CreatedAtAction("Get", new { id = experience.Id }, experience);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Experience experience)
+        {
+            var currentUserProfile = GetCurrentUserProfile();
+
+            if (currentUserProfile.Id != experience.UserProfileId)
+            {
+                return Unauthorized();
+            }
+
+            if (id != experience.Id)
+            {
+                return BadRequest();
+            }
+
+            _experienceRepository.Update(experience);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var currentUserProfile = GetCurrentUserProfile();
+            var experience = _experienceRepository.GetById(id);
+
+            if (currentUserProfile.Id != experience.UserProfileId)
+            {
+                return Unauthorized();
+            }
+
+            _experienceRepository.Delete(id);
+            return NoContent();
+        }
+
+        private UserProfile GetCurrentUserProfile()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+        }
+    }
+}
---
+++ ResumeRandomizer/Repositories/ExperienceRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResumeRandomizer.Data;
+using ResumeRandomizer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResumeRandomizer.Repositories
+{
+    public class ExperienceRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ExperienceRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Experience> GetByUserProfileId(int id)
+        {
+            return _context.Experience
+                .Where(e => e.UserProfileId == id)
+                .Include(e => e.Bullets)
+                .ToList();
+        }
+
+        public Experience GetById(int id)
+        {
+            return _context.Experience
+                .Include(e => e.Bullets)
+                .FirstOrDefault(e => e.Id == id);
+        }
+
+
+        public void Add(Experience experience)
+        {
+            _context.Add(experience);
+            _context.SaveChanges();
+        }
+
+        public void Update(Experience experience)
+        {
+            _context.Entry(experience).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var experience = GetById(id);
+            _context.Experience.Remove(experience);
+            _context.SaveChanges();
+        }
+    }
+}
